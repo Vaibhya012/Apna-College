4770920d35b43e2aa5579370710ca9d1ca5e0fdb
Vaibhya012 <gayakwadvaibhav120@gmail.com>
2024-08-03 17:45:11 +0530

    today start backend with node js and install funny npm packages

---
+++ Day19/index.js
@@ -0,0 +1,20 @@
+// // let n = 5;
+
+// // for(let i=0; i<n; i++){
+// //     console.log("Hello!", i);
+// // }
+
+// // let args = process.argv;
+
+// // for(let i=0; i<args.length; i++){
+// //     console.log("Hello to ", args[i]);
+// // }
+
+// const info = require("./Fruits");
+
+// console.log(info);
+// console.log(info[1]);
+
+import {sum, PI} from "./math.js";
+
+console.log(sum(1,2));
---
+++ Day19/math.js
@@ -0,0 +1,11 @@
+export const sum = (a, b) => a + b;
+export const mul = (a, b) => a * b;
+export const PI = 3.14;
+
+// let obj = {
+//     sum: sum,
+//     mul: mul,
+//     PI: PI
+// };
+
+// module.exports = obj;
